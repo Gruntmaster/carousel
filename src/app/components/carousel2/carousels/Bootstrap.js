9f0044d0264c7cc350f4dd73a3492ceb6850260f
Andrew Horlanov <andrew.horlanov@gmail.com>
2023-09-11 20:16:42 +0200

    BootstrapCarousel

--- src/app/components/carousel2/carousels/Bootstrap.js
+++ src/app/components/carousel2/carousels/Bootstrap.js
@@ -1,27 +1,48 @@
 "use client";
-import { useState } from "react";
-import { items } from "../public/Item.json";
+import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "../styles/Bootstrap.module.css";
+import styles from "./feedbackCard.module.css";
+import feedbackData from "../public/Item.json"; // Import the feedback data
 
 export default function BootstrapCarousel() {
-  const { bootstrap } = items;
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+
+  // Use the data from feedbackData.json
+  const { bootstrap } = feedbackData.items;
+
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       {bootstrap.map((item) => (
-        <Carousel.Item key={item.id} className={styles.itemP} interval={4000}>
-          <img src={item.imageUrl} alt="slides" />
-          <Carousel.Caption className={styles.caption}>
-            <h3>{item.title}</h3>
-            <p>{item.body}</p>
-            <button className="btn btn-danger">Visit Docs</button>
-          </Carousel.Caption>
+        <Carousel.Item
+          key={item.id}
+          className={styles.container}
+          interval={4000}
+        >
+          <img src={item.logoSrc} alt="logo" className={styles.logo} />
+          <div className={styles.line}></div>
+          <div className={styles.companyName}>{item.companyName}</div>
+          <img
+            src={item.projectImageSrc}
+            alt="energy"
+            className={styles.projectImage}
+          />
+          <div className={styles.skewed}></div>
+          <div className={styles.starRow}>{item.starRating}</div>
+          <div className={styles.skewed3}></div>
+          <div className={styles.date}>{item.date}</div>
+          <div className={styles.header1}>
+            Description:
+            <div className={styles.text1}>{item.description}</div>
+          </div>
+          <div className={styles.header2}>
+            Comment:
+            <div className={styles.text2}>{item.comment}</div>
+          </div>
         </Carousel.Item>
       ))}
     </Carousel>
